2ea6321851e38fb547e73daf340a5ec632b78994
Shitoss <95882382+Shitoss@users.noreply.github.com>
2021-12-11 18:29:12 +0300

    Update lab18.cpp

--- lab18/lab18.cpp
+++ lab18/lab18.cpp
@@ -22,21 +22,21 @@ int main()
 	cin >> n;
 
 	 fopen_s (&f,"f1.lab", "wb");
-	 fopen_s(&f1, "f2.lab", "wb");
+	 fopen_s(&f1, "f2.lab", "wb"); // открытие обоих файлов для записи
 
-	 for (int i = 0; i < n; i++)
+	 for (int i = 0; i < n; i++) // запись в файл с оценками
 	 {
 		 cout << "введите " << i + 1 << " оценку" << endl;
 		 cin >> m ;
 		 fwrite(&m, sizeof(int), 1, f1);
 
 	 }
-	 fclose(f1);
+	 fclose(f1); // закрытие файла с оценками
 
 
 	cout << "введите кол-во оценок за вступительные экзамены" << endl;
 	cin >> n1;
-	for (int i = 0; i < n1; i++)
+	for (int i = 0; i < n1; i++) //запись в файл с оценками за вступительные экзамены
 	{
 		cout << "введите " << i+1 << "оценку за вступит. экз." << endl;
 		cin >> a;
@@ -44,28 +44,27 @@ int main()
 
 	}
 
-	fseek(f, n1 * sizeof(int), SEEK_SET);
+	fseek(f, n1 * sizeof(int), SEEK_SET); 
 
-	fopen_s(&f1, "f2.lab", "rb");
+	fopen_s(&f1, "f2.lab", "rb"); // открытие файла с оценками на чтение
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) // запись оценок в конец  файла с оценками за вступит экзамены
 	{
 		d = fgetc(f1);
 		fwrite(&d, sizeof(int), 1, f);
 	}
 
 	fclose(f);
-	fclose(f1);
+	fclose(f1); // закрытие файлов
 
-	fopen_s(&f, "f1.lab", "rb");
-		if (f != NULL)
+	fopen_s(&f, "f1.lab", "rb");  // открытие файла на чтение
+		if (f != NULL) // проверка на пустоту файла
 		{
-			while (k = fread(&a, sizeof(int), 1, f))
+			while (k = fread(&a, sizeof(int), 1, f)) // вывод значений файла
 			{
 				cout << setw(7)<< a; 
 			}
 		}
-		fclose(f);
-
+		fclose(f); // закрытие файла
 
 }
